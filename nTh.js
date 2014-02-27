f29d888230eefc36bce2c44b560d6d5bb3a06303
David Perish <davidperish@outlook.com>
2014-02-27 10:37:05 -0500

    Update nTh.js
    Fixed issue with tween/teen numbers not showing th's as excepted.

--- nTh.js
+++ nTh.js
@@ -1,36 +1,46 @@
 /* nTh.js - is a javascript micro-library to return lower ordinals from integers  
-   version: 0.2
+   version: 0.2.1
    date: 2014-02-27
    license: Copyright (c) 2013 David Perish, MIT License */
+
 
 (function() {
  
   var nth = function (n) {
+    
     var intRegex = /^\d+$/;
+    
     if(intRegex.test(n)) {
-      var suffix = '';
-      if (n > 9 && n < 21 ) {
-         suffix = 'th'
+       
+      var suffix = 'th';
+      var nums = [11,12,13,14,15,16,17,18,19];
+      
+      for (var num in nums) {
+         if (n.toString().substr(n.toString().length -2, 2) == nums[num])
+            return n + suffix;
       }
-      else {
-         switch (n%10) {
-           case 1:
-             suffix = 'st';
-             break;
-           case 2:
-             suffix = 'nd';
-             break;
-           case 3:
-             suffix = 'rd';
-             break;
-           default:
-             suffix = 'th';
-             break;
-         }
+
+      switch (n%10) {
+        case 1:
+          suffix = 'st';
+          break;
+        case 2:
+          suffix = 'nd';
+          break;
+        case 3:
+          suffix = 'rd';
+          break;
+        default:
+          suffix = 'th';
+          break;
       }
+      
       return n + suffix;
+      
     }
+    
     return n;  
+    
   };
         
   window.nth = nth;
